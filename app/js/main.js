376cebc36e9d3de2eccd7c467d39e66db60381bd
Max Zhao <interarticle@gmail.com>
2015-06-08 17:16:33 +0800

    Very primitive demo

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@
  */
 "use strict";
 
-angular.module('SmartBattery', ['ngResource', 'uiGmapgoogle-maps', 'chart.js']);
+angular.module('SmartBattery', ['ngResource', 'uiGmapgoogle-maps', 'chart.js', 'ngAnimate']);
 
 angular.module('SmartBattery')
 .config(function(uiGmapGoogleMapApiProvider) {
@@ -33,14 +33,15 @@ angular.module('SmartBattery')
     }
 })
 .controller('VehicleMapsController', function($log, $scope, $interval, $http, $compile, VehicleMapsController$Options, uiGmapIsReady) {
-    var dummy_vehc = {latitude: 31.0232217, longitude: 121.4079586 };
-    $scope.map_options = {center: dummy_vehc, zoom: 13 };
+    var dummy_vehc = {latitude: 31.0268809, longitude: 121.4367119 };
+    $scope.map_options = {center: dummy_vehc, zoom: 15 };
     $scope.map_markers = [];
     $scope.map_marker_dict = {};
     $scope.gmap_markers = [];
     $scope.gmap_marker_dict = {};
     $scope.gmap = null;
     $scope.active_marker = null;
+    $scope.ready = false;
 
     function update_vehicle_status() {
         $http.get(VehicleMapsController$Options.vehicle_status_url)
@@ -64,7 +65,7 @@ angular.module('SmartBattery')
                 new_set.charts.series = ['SOC', 'Health'];
                 new_set.charts.data = [
                     [65, 59, 80, 81, 56, 55, 40],
-                    [28, 48, 40, 19, 86, 27, 90]
+                    [28, 48 * Math.random(), 40, 19, 86, 27, 90]
                 ];
                 new_set.data.state_of_charge = Math.random();
                 if (!$scope.map_marker_dict[veh.vehicle_id]) {
@@ -83,17 +84,22 @@ angular.module('SmartBattery')
                     $scope.gmap_markers.push(marker);
                     $scope.gmap_marker_dict[new_set.vehicle_id] = marker;
                     var elem = angular.element('#marker-label').children().clone();
+                    // XXX Register and wait for scope $destroy when marker
+                    // goes out of range
                     var new_scope = $scope.$new();
                     new_scope.vec = new_set;
                     new_scope.progress_breaks = VehicleMapsController$Options.progress_breaks;
                     $compile(elem)(new_scope);
+                    // XXX keep in mind that MarkerWithLabel with labelContent
+                    // being a DOM element does not work by default. A patch
+                    // is needed
                     marker.set('labelContent', elem[0]);
                     google.maps.event.addListener(marker, 'click', function() {
                         $scope.$apply(function() {
-                            if ($scope.active_marker == marker) {
+                            if ($scope.active_marker === new_set) {
                                 $scope.active_marker = null;
                             } else {
-                                $scope.active_marker = marker;
+                                $scope.active_marker = new_set;
                             }
                         });
                     });
@@ -101,7 +107,7 @@ angular.module('SmartBattery')
                         $scope.gmap_marker_dict[veh.vehicle_id].setPosition(new google.maps.LatLng(coords[0], coords[1]));
                     });
                     $scope.$watch('active_marker', function(amarker) {
-                        if (marker === amarker) {
+                        if (amarker === new_set) {
                             marker.setIcon(VehicleMapsController$Options.active_icon);
                         } else {
                             marker.setIcon(VehicleMapsController$Options.default_icon);
@@ -120,6 +126,8 @@ angular.module('SmartBattery')
     uiGmapIsReady.promise(1).then(function(maps) {
         $scope.gmap = maps[0];
         $interval(update_vehicle_status, VehicleMapsController$Options.vehicle_status_interval);
+        update_vehicle_status();
+        $scope.ready = true;
         $log.log($scope.gmap);
     });
 })
