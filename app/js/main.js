ea546ddab20a07b4d38e8de9ef7a9da04a02ad78
Max Zhao <interarticle@gmail.com>
2015-06-16 17:23:38 +0800

    Custom Script Tag allowing asynchronous gmaps loading

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,8 @@ angular.module('SmartBattery')
         size: {width: 48, height: 48},
         origin: {x: 0, y: 0},
         anchor: {x: 48/2, y: 48/2}
-    }
+    },
+    gmaps_load_timeout: 10000
 })
 .factory('VMC$Tools', function($log, $compile) {
     return {
@@ -93,7 +94,7 @@ angular.module('SmartBattery')
         }
     };
 })
-.controller('VehicleMapsController', function($q, $log, $scope, $interval, $http, $compile, VehicleMapsController$Options, VMC$Tools) {
+.controller('VehicleMapsController', function($q, $log, $scope, $interval, $timeout, $http, $compile, VehicleMapsController$Options, VMC$Tools) {
     var dummy_vehc = {latitude: 31.0268809, longitude: 121.4367119 };
     $scope.map_options = {center: dummy_vehc, zoom: 2 }; // 15 };
     $scope.map_markers = [];
@@ -224,8 +225,16 @@ angular.module('SmartBattery')
         });
     }
 
-    load_maps();
-    start_maps();
+    $scope.maps_loaded = function() {
+        $timeout.cancel(gmaps_load_timeout);
+        load_maps();
+        start_maps();
+    }
+
+    $scope.gmaps_load_failed = false;
+    var gmaps_load_timeout = $timeout(function() {
+        $scope.gmaps_load_failed = true;
+    }, VehicleMapsController$Options.gmaps_load_timeout);
 })
 .directive('coloredProgress', function() {
     return {
@@ -235,6 +244,116 @@ angular.module('SmartBattery')
         scope: {
             value: '=value',
             progress_breaks: '=progressbreaks',
+        }
+    };
+})
+.directive('sbfScriptLoader', function($log) {
+    // For global callback functions
+    var directive_counter = 0;
+    /**
+     * on-loaded: command to run when loading completes
+     * loaded: sets to true when loaded
+     * src: url to load the script
+     * callback: substring in src to substitute for the name of a global
+     *  callback function
+     * load: set to true will trigger loading
+     * load-on-ready: if attribute is present, automatically triggers loading
+     *  on document ready
+     * error: set to true when loading fails
+     * onerror: executes when loading fails
+     *
+     * Note: this is a one-shot loader.  If loading fails, will not retry.
+     */
+    return {
+        restrict: 'AE',
+        replace: true,
+        scope: {
+            'onloaded': '&onLoaded',
+            'loaded': '=?',
+            'src': '@',
+            'callback': '@',
+            'load': '=?',
+            'error': '=?',
+            'onerror': '&onError'
+        },
+        link: function($scope, $element, $attr) {
+            var instance_number = directive_counter;
+            directive_counter ++;
+            if (($attr.load == null) && ($attr.loadOnReady == null)) {
+                throw new Error("You must specify either load or load-on-ready");
+            }
+            if ($attr.useScriptTag != null && $attr.callback == null) {
+                throw new Error('It is unsafe to use script tag loading without a callback, will not be able to determine when loading is complete. Refusing to continue');
+            }
+            var element = angular.element(document.createComment(
+                ' sbf-script-loader: ' + $scope.src + ' '
+            ));
+            $element.replaceWith(element);
+
+            $scope.loaded = false;
+            $scope.error = false;
+
+            function load_script() {
+                // Disable function to prevent reloading (accidental or otherwise)
+                load_script = null;
+
+                var url = $scope.src;
+
+                function on_loaded() {
+                    $scope.$apply(function() {
+                        $scope.loaded = true;
+                        $scope.onloaded();
+                    });
+                }
+
+                if ($scope.callback) {
+                    var callback_name = '_sbfScriptLoader_callback_' + instance_number.toString();
+                    window[callback_name] = function() {
+                        on_loaded();
+                        delete window[callback_name];
+                    }
+
+                    url = url.replace($scope.callback, callback_name);
+                }
+
+
+                if ($attr.useScriptTag == null) {
+                    $log.log('loading ' + String(url) + ' with jQuery');
+                    jQuery.ajax({
+                        dataType: 'script',
+                        cache: true,
+                        url: url
+                    }).done(function() {
+                        on_loaded();
+                    }).fail(function() {
+                        $scope.$apply(function() {
+                            $scope.error = true;
+                            $scope.onerror();
+                        });
+                    });
+                } else {
+                    $log.log('loading ' + String(url) + ' with <script>');
+                    var scriptTag = angular.element('<script>');
+                    scriptTag.attr('src', url);
+                    scriptTag.attr('type', 'text/javascript');
+                    element.replaceWith(scriptTag);
+                }
+            }
+
+            var unwatch_load = $scope.$watch('load', function(load) {
+                if (load) {
+                    unwatch_load();
+                    load_script();
+                }
+            });
+
+            if ($attr.loadOnReady != null) {
+                angular.element(document).ready(function() {
+                    $scope.$apply(function() {
+                        $scope.load = true;
+                    });
+                });
+            }
         }
     };
 })
