5b51a700df5bab9925a26533b354881113f74b6b
Max Zhao <interarticle@gmail.com>
2015-06-22 23:41:10 +0800

    ui router and vehicle details page

--- app/js/app/SmartBatteryFrontend/VehicleDetails/index.js
+++ app/js/app/SmartBatteryFrontend/VehicleDetails/index.js
@@ -3,4 +3,19 @@
  * @author Yichen Zhao
  * @license Proprietary
  */
-angular.module('sbfModuleVehicleDetails', ['ngResource', 'chart.js', 'ngAnimate', 'iaUtils', 'ui.bootstrap', 'ui.router']);
+angular.module('sbfModuleVehicleDetails', ['ngResource', 'chart.js', 'ngAnimate', 'iaUtils', 'ui.bootstrap', 'ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/wrong_url');
+    $stateProvider
+        .state('wrong_url', {
+            url: '/wrong_url',
+            template: angular.element('#wrong-url-template').html(),
+        })
+        .state('vehicle_details', {
+            url: '/vehicle_details/{vehicle_id}',
+            template: angular.element('#vehicle-details-template').html(),
+            controller: 'sbfVehicleDetailsController'
+        })
+    ;
+})
+;
