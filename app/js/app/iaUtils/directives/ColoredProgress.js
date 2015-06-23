40cdb603044118fbbd6b73877b2d98dfb1307ea0
Max Zhao <interarticle@gmail.com>
2015-06-23 20:50:56 +0800

    iaucolored progress reversal/perf boost

--- app/js/app/iaUtils/directives/ColoredProgress.js
+++ app/js/app/iaUtils/directives/ColoredProgress.js
@@ -2,40 +2,29 @@ angular.module('iaUtils')
 .directive('iauColoredProgress', function() {
     var progress_classes = ['progress-bar-danger', 'progress-bar-warning', 'progress-bar-success'];
     return {
-        restrict: 'A',
+        template: "<div ng-class=\"{'progress-bar': true, 'progress-bar-success': value >= progress_breaks[1], 'progress-bar-warning': value >= progress_breaks[0] && value < progress_breaks[1], 'progress-bar-danger': value < progress_breaks[0]}\" ng-style=\"{width: !vertical ? value * 100 + '%' : null, height: vertical ? value * 100 + '%' : null}\"></div>",
+        restrict: 'AE',
+        replace: true,
         scope: {
             value: '=ratio',
             progress_breaks: '=?progressbreaks',
         },
         link: function($scope, $element, $attr) {
-            var vertical;
-            vertical = false;
             if (!$attr.progressbreaks) {
                 $scope.progress_breaks = [0.2, 0.8];
             }
-
-            if ($attr.vertical != null) {
-                vertical = true;
-            }
-
-            $element.addClass('progress-bar');
-
-            $scope.$watch('value', function(value) {
-                var active_index = 0;
-                var i, l;
-                for (i=0, l=$scope.progress_breaks.length; i < l; i++) {
-                    if (value >= $scope.progress_breaks[i]) {
-                        active_index = i + 1;
-                    } else {
-                        break;
-                    }
-                }
-                for (i=0, l=progress_classes.length; i < l; i++) {
-                    $element.toggleClass(progress_classes[i], i == active_index);
-                }
-
-                $element.css(vertical ? 'height' : 'width', value * 100 + '%');
-            });
-        },
+            $scope.vertical = ($attr.vertical != null);
+            // Note: it is totally unnecessary to create a complex link function
+            // just for the sake of propagating class/etc attributes on the
+            // original element. These values are automatically propagated
+            // to the replaced top level tag by Angular automatically. See the
+            // angular ui bootstrap tab directive for example.
+            //
+            // This seems to be slightly more efficient than manual parsing in
+            // terms of $watch performance.  Performance may be further boosted
+            // by dynamically regenerating the template to suit vertical and
+            // non-vertical needs, using $compile, but that's much more complex
+            // due to the need to manually propagate $attr etc.
+        }
     };
 });
