42b5b9f97d2feb0dd78bcd33b18ac338e78d9234
Max Zhao <interarticle@gmail.com>
2015-07-01 18:04:43 +0800

    Reorganize scripts to SPA (move SPA settings from VD specific modules to indx

---
+++ app/js/app/SmartBatteryFrontend/Index/index.js
@@ -0,0 +1,40 @@
+angular.module('sbfModuleTemplates', []);
+angular.module('sbfModuleIndex', ['sbfModuleVehiclesMap', 'sbfModuleVehicleDetails', 'sbfModuleTemplates'])
+.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/wrong_url');
+    $stateProvider
+        .state('wrong_url', {
+            url: '/wrong_url',
+            templateUrl: 'pages/wrong_url.html',
+            data: {
+                title: 'Wrong URL'
+            }
+        })
+        .state('vehicle_details', {
+            url: '/vehicle_details/{vehicle_id}',
+            templateUrl: 'pages/vehicle_details.html',
+            controller: 'sbfVehicleDetailsController',
+            data: {
+                title: "Vehicle Details"
+            }
+        })
+        .state('vehicles_map', {
+            url: '/vehicles_map',
+            templateUrl: 'pages/vehicles_map.html',
+            controller: 'sbfVehicleMapsController',
+            data: {
+                title: 'Vehicles Map'
+            }
+        })
+    ;
+})
+.run(function($rootScope) {
+    $rootScope.$on('$stateChangeSuccess', function(event, toState) {
+        var title;
+        if (toState.data) {
+            title = toState.data.title;
+        }
+        $rootScope.page_title = title;
+    });
+})
+;
